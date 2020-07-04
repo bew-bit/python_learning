eba041d973105b14be3b07448d0667d33f228461
Svetlana <svetlana.reimer.job@gmail.com>
2020-07-04 15:48:19 +0300

    Урок 6. Объектно-ориентированное программирование 5. Реализовать класс Stationery (канцелярская принадлежность). Определить в нем атрибут title (название) и метод draw (отрисовка). Метод выводит сообщение “Запуск отрисовки.” Создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер). В каждом из классов реализовать переопределение метода draw. Для каждого из классов методы должен выводить уникальное сообщение. Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
    Урок 7. ООП. Продвинутый уровень
    1. Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса (метод __init__()), который должен принимать данные (список списков) для формирования матрицы.
    Подсказка: матрица — система некоторых математических величин, расположенных в виде прямоугольной схемы.
    Примеры матриц вы найдете в методичке.
    Следующий шаг — реализовать перегрузку метода __str__() для вывода матрицы в привычном виде.
    Далее реализовать перегрузку метода __add__() для реализации операции сложения двух объектов класса Matrix (двух матриц). Результатом сложения должна быть новая матрица.
    Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой матрицы складываем с первым элементом первой строки второй матрицы и т.д.
    2. Реализовать проект расчета суммарного расхода ткани на производство одежды. Основная сущность (класс) этого проекта — одежда, которая может иметь определенное название. К типам одежды в этом проекте относятся пальто и костюм. У этих типов одежды существуют параметры: размер (для пальто) и рост (для костюма). Это могут быть обычные числа: V и H, соответственно.
    Для определения расхода ткани по каждому типу одежды использовать формулы: для пальто (V/6.5 + 0.5), для костюма (2 * H + 0.3). Проверить работу этих методов на реальных данных.
    Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания: реализовать абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property.

--- work#6.py
+++ work#6.py
@@ -175,3 +175,47 @@ print(Aston_Martin_Vanquish.show_speed())
 print(Kia_Rio.show_speed())
 print(Citroen_Jumpy.show_speed())
 print(Chevrolet_Camaro_SS.show_speed())
+
+#5. Реализовать класс Stationery (канцелярская принадлежность).
+#Определить в нем атрибут title (название) и метод draw (отрисовка).
+#Метод выводит сообщение “Запуск отрисовки.” Создать три дочерних
+#класса Pen (ручка), Pencil (карандаш), Handle (маркер). В каждом
+#из классов реализовать переопределение метода draw. Для каждого
+#из классов методы должен выводить уникальное сообщение. Создать
+#экземпляры классов и проверить, что выведет описанный метод для
+#каждого экземпляра.
+class Stationary:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return f'Начало рисования {self.title}'
+
+class Pen(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'{self.title}. Начало рисования ручкой'
+
+class Pencil(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'{self.title}. Начало рисования карандашом'
+
+class Handle(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'{self.title}. Начало рисования маркером'
+
+pen = Pen('Ручка')
+pencil = Pencil('Карандаш')
+handle = Handle('Маркер')
+print('\n5.')
+print(pen.draw())
+print(pencil.draw())
+print(handle.draw())
